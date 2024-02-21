07c7662f974b955b4acb627971dce1a95b9c85fc
lizeren <li3103@purdue.edu>
2024-02-21 02:37:06 -0500

    tune the parameters. It works well

--- FlushReload.c
+++ FlushReload.c
@@ -3,12 +3,13 @@
 #include <stdint.h>
 #include <inttypes.h>
 
-uint8_t array[256 * 64];
+#define stride (256)
+uint8_t array[256 * stride];
 volatile uint8_t temp;
-unsigned char secret = 64;
+unsigned char secret = 72;
 /* cache hit time threshold assumed*/
 #define CACHE_HIT_THRESHOLD (130)
-#define DELTA 0
+#define DELTA 64
 
 static inline uint64_t read_pmccntr(void)
 {
@@ -18,7 +19,7 @@ static inline uint64_t read_pmccntr(void)
 }
 void victim()
 {
-  temp = array[secret * 64 + DELTA];
+  temp = array[secret * stride + DELTA];
 }
 void flushSideChannel()
 {
@@ -26,13 +27,13 @@ void flushSideChannel()
   // Write to array to bring it to RAM to prevent Copy-on-write
   for (i = 0; i < 256; i++)
   {
-    array[i * 64 + DELTA] = 1;
+    array[i * stride + DELTA] = 1;
   }
 
   // flush the values of the array from cache
   for (i = 0; i < 256; i++)
   {
-    asm volatile("dc civac, %0" : : "r"(&array[i * 64 + DELTA]) : "memory");
+    asm volatile("dc civac, %0" : : "r"(&array[i * stride + DELTA]) : "memory");
     asm volatile("dsb ish"); // Data synchronization barrier ensure write has completed
     asm volatile("isb");
   }
@@ -50,7 +51,7 @@ void reloadSideChannel()
     time1 = read_pmccntr();
     asm volatile("isb");
 
-    temp = array[mix_i * 64 + DELTA];
+    temp = array[mix_i * stride + DELTA];
     asm volatile("dsb ish"); // Data synchronization barrier after volatile access
     asm volatile("isb");
 
@@ -61,7 +62,8 @@ void reloadSideChannel()
     // printf("~~~~~At mix_i = %d~~~~~\ntime_diff: %d\n\n", mix_i, time_diff);
     if (time_diff <= CACHE_HIT_THRESHOLD)
     {
-      printf("array[%d*128 + %d] is in cache.\n", mix_i, DELTA);
+      // printf("array[%d*%d + %d] is in cache at i = %d.\n", mix_i, stride, DELTA,i);
+      printf("array[%d*%d + %d] is in cache.\n", mix_i, stride, DELTA);
       printf("The Secret = %d.\n", mix_i);
       printf("hit time is %d\n",time_diff);
       printf("\n");
@@ -82,7 +84,7 @@ void reloadSideChannel()
 
 int main(int argc, const char **argv)
 {
-  for (int i = 0; i < 256 * 64; i++)
+  for (int i = 0; i < 256 * stride; i++)
   {
     array[i] = i;
   }
